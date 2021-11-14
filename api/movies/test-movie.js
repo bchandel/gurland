9073f9cd04e8b7b9a3eb06d187c1eae585bdb30a
balkaran0810 <balkaran@reachhcs.com>
2021-11-14 19:10:05 +0530

    BS added test cases for movies

--- api/movies/test-movie.js
+++ api/movies/test-movie.js
@@ -37,6 +37,38 @@ describe('Record Module', () => {
 			});
 		});
 		
+		it("should add movie", () => {
+			let movieService =  movie.movieService(db);
+			let payload = {
+				"name" : "test123", 
+				"description": "test movie",
+				"release_date": "11-10-2021", 
+				"genres": [{
+					"name": "Comedy test"
+				}], 
+				"duration": "00:51:00",
+				"rating": "3.4/5"
+		   };
+			return movieService.addMovie(payload).then((record) => {
+				expect(record).to.have.property("_id");
+				expect(record).to.have.property("name");
+				expect(record).to.have.property("description");
+				expect(record).to.have.property("release_date");
+				expect(record).to.have.property("genres");
+				expect(record).to.have.property("duration");
+				expect(record).to.have.property("rating");
+			});
+		});
+
+		it("should remove movie by name", () => {
+			let movieService =  movie.movieService(db);
+			let name =  "test123" ;
+			return movieService.removeMovieByName(name).then((record) => {
+				expect(record).to.have.property("n").equal(1);
+				expect(record).to.have.property("ok");
+				expect(record).to.have.property("deletedCount").equal(1);
+			});
+		});
 	});
 
 	describe('Test /api/movie/get-all-movies RESTAPI', () => {
@@ -53,5 +85,51 @@ describe('Record Module', () => {
 				done();
 			});
 		});
+	});
+
+	describe('Test /api/movie/add-movie RESTAPI', () => {
+
+		it('it should add gener', (done) => {
+			let payload = {
+				"name" : "test123", 
+				"description": "test movie",
+				"release_date": "11-10-2021", 
+				"genres": [{
+					"name": "Comedy test"
+				}], 
+				"duration": "00:51:00",
+				"rating": "3.4/5"
+		   	};
+			chai.request(server)
+			.post('/api/movie/add-movie')
+			.send(payload)
+			.end((err, record) => {
+				expect(record.body).to.have.property("success");
+				expect(record.body).to.have.property("data").to.be.a('object');
+				expect(record.body.success).to.be.equal(true);
+				done();
+			});
+		});
+
+		
+	});
+
+	describe('Test /api/movie/remove-movie/:name RESTAPI', () => {
+
+		it('it should remove gener by name', (done) => {
+			let name = "test123"
+			chai.request(server)
+			.delete('/api/movie/remove-movie/'+name)
+			.send()
+			.end((err, record) => {
+				expect(record.body).to.have.property("succes");
+				expect(record.body).to.have.property("message");
+				expect(record.body.succes).to.be.equal(true);
+				expect(record.body.message).to.be.equal(`Movie deleted!`);
+				done();
+			});
+		});
+
+		
 	});
 });
